93dae53780a5899f55e6f78c2eab8b94eedf4eb5
Kevin Vo <vodangkhoa@gmail.com>
2018-05-04 16:23:12 +0700

    refactor constants

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import MovieContainer from './MovieContainer';
 
+const apiKey = `8237b2eb1c4e5ddc3156da0988afdec2`
 export default class App extends Component {
 
   constructor(props) {
     super(props)
     this.state = {}
-    const apiKey = `8237b2eb1c4e5ddc3156da0988afdec2`
     this.movieFetchUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
     this.movieSearchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1`
   }
--- src/MoviePost.js
+++ src/MoviePost.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './MoviePost.css';
 
+const imagePrefix = `https://image.tmdb.org/t/p/w342/`
 export default class MoviePost extends React.Component {
 
   constructor(props) {
     super(props)
-    this.imagePrefix = `https://image.tmdb.org/t/p/w342/`
   }
 
   toggleElement(event) {
   }
 
   render() {
-    const post_path = `${this.imagePrefix}${this.props.movie.poster_path}`
+    const post_path = `${imagePrefix}${this.props.movie.poster_path}`
     return (
       <div className='movie-item'>
         <img src={post_path}/>
